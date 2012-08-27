b2e48347273e6bca9e1ec48371263112887f527a
Oliver Brook <oliver@potatolondon.com>
2012-08-27 23:13:49 +0100

    Added a partial malloc implementation with some real confusion as to how 'null' is supposed to behave in the Jack language!

--- scripts/build_project_12.py
+++ scripts/build_project_12.py
@@ -10,18 +10,21 @@ import glob
 ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 class_names = [
-    'Array',
-    'Keyboard',
-    'Math',
     'Memory',
-    'Output',
-    'Screen',
-    'String',
-    'Sys',
+    # 'Array',
+    # 'Keyboard',
+    # 'Math',
+    # 'Output',
+    # 'Screen',
+    # 'String',
+    # 'Sys',
 ]
 
 tecs_files = os.path.expanduser(os.path.join('~', 'tecs-software-suite-2.5'))
 supplied_os_files = glob.glob(os.path.join(tecs_files, 'OS', '*.*'))
+
+new_os_dir = os.path.join(ROOT, '12', 'os')
+
 jack_compiler = os.path.join(tecs_files, 'JackCompiler.sh')
 vm_emulator = os.path.join(tecs_files, 'VMEmulator.sh')
 
@@ -31,8 +34,14 @@ for class_name in class_names:
     test_script = os.path.join(path, class_name + 'Test.tst')
     print(path)
     
-    print('Copying OS')
+    print('Copying supplied OS files')
     cp_args = ['cp'] + supplied_os_files
+    cp_args.append(path)
+    subprocess.call(cp_args)
+    
+    print('Copying class file')
+    cp_args = ['cp']
+    cp_args.append(os.path.join(new_os_dir, class_name + '.jack'))
     cp_args.append(path)
     subprocess.call(cp_args)
     
@@ -47,4 +56,3 @@ for class_name in class_names:
         print('Class "%s" requires manual testing.' % class_name)
     
     print('\n')
- 
