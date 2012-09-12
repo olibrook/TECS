cab0b08dd31b44a7a4efdbeaf07b47d2374b0f15
Oliver Brook <oliver@potatolondon.com>
2012-09-12 23:07:49 +0100

    Started work on the Math functions, with implementation of bit()

--- scripts/build_project_12.py
+++ scripts/build_project_12.py
@@ -10,10 +10,10 @@ import glob
 ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 class_names = [
-    'Memory',
+    # 'Memory',
     # 'Array',
     # 'Keyboard',
-    # 'Math',
+    'Math',
     # 'Output',
     # 'Screen',
     # 'String',
