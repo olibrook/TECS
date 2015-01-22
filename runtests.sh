c652b90e79b8c56d3eacbf5588d565e706414eee
Oliver Brook <olibrook@gmail.com>
2015-01-22 12:06:42 +0000

    Fix three year old CPU bug. JGT instructions were jumping when val=0, must check not-negative AND not-zero.

--- runtests.sh
+++ runtests.sh
@@ -1,10 +1,47 @@
 #!/usr/bin/env sh
 
-E1=(src/01/*.tst)
-E2=(src/02/*.tst)
-E3=(src/03/**/*.tst)
-HARDWARE_EXCERCISES=( ${E1[@]} ${E2[@]} ${E3[@]})
+HARDWARE_EXCERCISES=(
+	src/01/And16.tst
+	src/01/And.tst
+	src/01/DMux4Way.tst
+	src/01/DMux8Way.tst
+	src/01/DMux.tst
+	src/01/Mux16.tst
+	src/01/Mux4Way16.tst
+	src/01/Mux8Way16.tst
+	src/01/Mux.tst
+	src/01/Not16.tst
+	src/01/Not.tst
+	src/01/Or16.tst
+	src/01/Or8Way.tst
+	src/01/Or.tst
+	src/01/Xor.tst
+	src/02/Add16.tst
+	src/02/ALU.tst
+	src/02/FullAdder.tst
+	src/02/HalfAdder.tst
+	src/02/Inc16.tst
+	src/03/a/Bit.tst
+	src/03/a/PC.tst
+	src/03/a/RAM64.tst
+	src/03/a/RAM8.tst
+	src/03/a/Register.tst
+	src/03/b/RAM16K.tst
+	src/03/b/RAM4K.tst
+	src/03/b/RAM512.tst
+	src/05/ComputerAdd-external.tst
+	src/05/ComputerAdd.tst
+	src/05/ComputerMax-external.tst
+	src/05/ComputerMax.tst
+	src/05/ComputerRect-external.tst
+	src/05/ComputerRect.tst
+	src/05/CPU-external.tst
+	src/05/CPU.tst
+)
 
+INTERACTIVE_TESTS=(
+	src/05/Memory.tst
+)
 
 for test_file in ${HARDWARE_EXCERCISES[@]}; do
 	echo $test_file
